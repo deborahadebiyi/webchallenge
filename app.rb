8e406ccafbbf9b39d3b9123fd504613eb5f2dc1d
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-10-28 04:31:34 +0000

    Posted params

--- app.rb
+++ app.rb
@@ -19,7 +19,11 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @random_name = params[:name]
   erb(:index)
